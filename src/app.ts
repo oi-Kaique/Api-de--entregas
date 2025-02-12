71460c7142250563cfb45855d60211d0c93ac100
Kaique <kaiquesouzapereira63@gmail.com>
2025-02-12 14:51:35 -0300

    create routes and delivers

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express"
 import "express-async-errors"
 
+
+import { routes } from "./routes"
 import { errorHandling } from "./middlewares/error-handling"
 import { error } from "console"
 
 const app = express()
 
 app.use(express.json())
+app.use(routes)
 
 app.use(errorHandling)
 
---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+class UsersController{  
+  
+  create( request: Request, response: Response ){
+    return response.json({ message: "ok"})
+  }
+
+}
+
+export { UsersController }
