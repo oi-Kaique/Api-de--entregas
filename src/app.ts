9add3d85e87eaf33cbdf8db377cf11b9e710b868
Kaique <kaiquesouzapereira63@gmail.com>
2025-02-12 14:38:04 -0300

    configuração2

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from "express"
+import "express-async-errors"
+
+import { errorHandling } from "./middlewares/error-handling"
+import { error } from "console"
 
 const app = express()
 
 app.use(express.json())
+
+app.use(errorHandling)
 
 export { app }
