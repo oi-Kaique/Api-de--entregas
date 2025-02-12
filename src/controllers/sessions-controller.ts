606ea22a95407f2b3cc6eb36c5d27dee4319d392
Kaique <kaiquesouzapereira63@gmail.com>
2025-02-12 17:06:48 -0300

    recover user

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,7 +1,32 @@
 import { Request, Response } from "express"
+import { AppError } from "@/utils/appError"
+import { prisma } from "@/database/prisma"
+import { compare } from "bcrypt"
+import { z } from "zod"
+import { cp } from "fs"
+
 
 class SessionsController {
-  create(request: Request, response: Response){
+ async create(request: Request, response: Response){
+
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    })
+
+    const { email, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({ where: {email} })
+
+    if (!user) {
+      throw new AppError("Invalid email or password", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new AppError("Invalid email or password", 401)
+    }
 
     return response.json()
 
