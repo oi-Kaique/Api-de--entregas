8bb086e119f7a2c3a7849991061ee6afea5d8b88
Kaique <kaiquesouzapereira63@gmail.com>
2025-02-14 16:26:35 -0300

    Created controller routes the deliveries

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,11 @@ import { Router } from "express"
 import { userRoutes } from "./users-routes"
 import { sessionsRoutes } from "./sessions-routes"
 import { deliveriesRoutes } from "./deliveries-routes"
-
+import { deliveryLogsRoutes } from "./delivery-logs-routes" 
 
 const routes = Router()
 routes.use("/users", userRoutes)
 routes.use("/sessions", sessionsRoutes)
 routes.use("/deliveries", deliveriesRoutes)
+routes.use("/delivery-logs", deliveryLogsRoutes)
 export { routes }
