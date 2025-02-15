4666227ff1693c35d81bab6cd16957526760387f
Kaique <kaiquesouzapereira63@gmail.com>
2025-02-14 21:39:08 -0300

    testing register the user

--- src/tests/usres-controller.test.ts
+++ src/tests/usres-controller.test.ts
@@ -28,4 +28,16 @@ describe("UserController", () => {
     user_id = response.body.id
   })
 
+  it("should throw an error if user with same email aleready exists", async () => {
+    
+    const response = await request(app).post("/users").send({
+      name: "Duplicate User",
+      email: "testuser@example.com",
+      password: "password123",
+    })
+
+    expect(response.status).toBe(400)
+    expect(response.body.message).toBe("User with same email already exists")
+  })
+
 })
